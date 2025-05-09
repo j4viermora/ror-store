5d7e8e6596b060e1a49c5029c6ece47921d5285d
Javier A Mora gil <j4viermora@gmail.com>
2025-05-09 16:38:14 -0300

    feat: test stuffs

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,16 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
+
+  test "send email notification when back in stock" do
+    product = products(:tshirt)
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
   # test "the truth" do
   #   assert true
   # end
